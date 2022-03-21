e87be4584d5190caa1996ca8f9257cda6cb626dd
lambda shi <lambda7xx@gmail.com>
2022-03-21 13:00:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 WORKER_IP ="127.0.0.1"#workflow机器的ip 
 
 key1 = "key1"
-value1 = "1" * 1000000
+value1 = "1" * 10000
 
 key2 ="key2"
 
-value2 = "2" * 1500000
+value2 = "2" * 15000
 
 key3 = "key3"
-value3 = "3" * 1800000
+value3 = "3" * 18000
 
 keys =[] 
 key = "key"
@@ -16,8 +16,8 @@ key = "key"
 values  = [] 
 value = "1".encode() #转为bytes 
 
-maxSize = 10000000
-for i in range(10000):
+maxSize = 100000
+for i in range(1000):
     tkey = key + str(i)
     tvalue = value *  maxSize
     keys.append(tkey)
